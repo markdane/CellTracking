0363b8766aae4985554c31c3b38bb9f025cd2879
markdane <dane@ohsu.edu>
2018-10-10 10:07:20 -0700

    aligning nucleartracking masks with cytoplasmic masks. Needs more debug and being shelved for other priorities.

--- Analyze_cyto.R
+++ Analyze_cyto.R
@@ -5,10 +5,15 @@ getGFPData <- function(redDirPath, GFPDirPath){
     readImage()
   GFP <- dir(GFPDirPath, pattern = "Scene1Interval.*_GFP.tif", full.names = TRUE) %>%
     readImage()
-  nuc_mask <- dir(redDirPath, pattern = "Scene1Interval0________Tracking-Result_.*", full.names = TRUE) %>%
-    readImage() %>%
-    bwlabel()
+  nuc_mask_dble <- dir(paste0(redDirPath,""), pattern = ":Users", full.names = TRUE) %>%
+    readImage()
+  imageData(nuc_mask_dble) <- imageData(nuc_mask_dble)*2^16
+
+  nuc_mask_labels <- lapply(1:dim(nuc_mask_dble)[3], function(i){
+    apply(imageData(nuc_mask_dble[,,i]), 2, as.integer)
+  })
   
+  nuc_mask <- Image(unlist(nuc_mask_labels), dim = c(dim(nuc_mask_labels[[1]]),length(nuc_mask_labels)))
   # cells_Hela <- rgbImage(green = 1.5*cel_Hela, blue = nuc_Hela)
   # display(cells_Hela, all = TRUE)
   # display(colorLabels(nmask_Hela), all=TRUE)
@@ -17,31 +22,36 @@ getGFPData <- function(redDirPath, GFPDirPath){
   cell_mask = propagate(GFP, seeds=nuc_mask, mask=ctmask)
   cyto_mask <- cell_mask - nuc_mask
   
+  #display(colorLabels(red), all=TRUE)
+  #display(colorLabels(nuc_mask_dble), all=TRUE)
+  #display(colorLabels(nuc_mask), all=TRUE)
   #display(colorLabels(cyto_mask), all=TRUE)
+  #display(colorLabels(cell_mask), all=TRUE)
   # segmented = paintObjects(cmask, cells_Hela, col='#ff00ff')
   # segmented = paintObjects(nmask_Hela, segmented, col='#ffff00')
   # display(segmented, all=TRUE)
-  GFP_data <- lapply(1:dim(cell_mask)[3], function(slice){
-    cell_data <-     computeFeatures.basic(cell_mask[,,slice], GFP[,,slice]) %>%
+  GFP_data <- lapply(1:(dim(cell_mask)[3]), function(frame){
+    cell_data <- computeFeatures.basic(cell_mask[,,frame], GFP[,,frame]) %>%
       as.tibble() %>%
       rename_at(names(.), funs( paste0("Cell_",.)))
-    cell_data <- tibble(Slice = slice, TrackId = 1:nrow(cell_data)) %>%
+    cell_data <- tibble(frame = frame-1 , trackId_nuclear = 1:nrow(cell_data)) %>%
       bind_cols(cell_data)
-    nuclear_data <- computeFeatures.basic(nuc_mask[,,slice], GFP[,,slice]) %>%
+    nuclear_data <- computeFeatures.basic(nuc_mask[,,frame], GFP[,,frame]) %>%
       as.tibble() %>%
       rename_at(names(.), funs( paste0("Nuclei_",.) ) ) %>%
-      mutate(Slice = slice,
-             TrackId = 1:nrow(.))
-    cyto_data <- computeFeatures.basic(cyto_mask[,,slice], GFP[,,slice]) %>%
+      mutate(frame = frame -1,
+             trackId_nuclear = 1:nrow(.))
+    cyto_data <- computeFeatures.basic(cyto_mask[,,frame], GFP[,,frame]) %>%
       as.tibble() %>%
       rename_at(names(.), funs( paste0("Cyto_",.) ) )%>%
-      mutate(Slice = slice,
-             TrackId = 1:nrow(.))
+      mutate(frame = frame -1,
+             trackId_nuclear = 1:nrow(.))
     all_data <- cell_data %>%
-      left_join(nuclear_data, by = c("Slice", "TrackId")) %>%
-      left_join(cyto_data, by = c("Slice", "TrackId"))
+      left_join(nuclear_data, by = c("frame", "trackId_nuclear")) %>%
+      left_join(cyto_data, by = c("frame", "trackId_nuclear"))
   }) %>%
     bind_rows()
 }
 
-all_data <- getGFPData(redDirPath = "Beacon-42/Red_Reg",GFPDirPath = "Beacon-42/GFP_Reg")
+cyto_data <- getGFPData(redDirPath = "Beacon-42/Red_Reg",GFPDirPath = "Beacon-42/GFP_Reg")
+write_csv(cyto_data, "Beacon-42/Cyto_data.csv")
