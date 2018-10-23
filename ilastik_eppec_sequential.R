fc44163429ab060919e364d9a1a91b0e3946578c
markdane <dane@ohsu.edu>
2018-10-23 14:47:50 -0700

    Added readonly=True flag still need to make parallel calls for each image sequence

--- ilastik_eppec_sequential.R
+++ ilastik_eppec_sequential.R
@@ -4,17 +4,17 @@ library(stringr)
 #that have TxRed_Reg and GFP_Reg subdirectories
 #####DEBUG skipping over first two subdirectories
 pwd <- getwd()
-dirs <- paste0(pwd,"/",dir(pattern = "[[:alnum:]]*_[[:digit:]]*"))[2:12]
-#dir_path = "A1_1
+dirs <- paste0(pwd,"/",dir(pattern = "[[:alnum:]]*_[[:digit:]]*"))[5]
+#dir_path = "A3_1"
 
 res <- lapply(dirs, function(dir_path){
   message("processing images in: ",dir_path)
   well_location <- str_remove(dir_path,".*/")
   
-  system(paste0('srun -c 8 /home/users/dane/ilastik-1.3.2b3-Linux/run_ilastik.sh --headless --project=/graylab/share/dane/CellTracking/HeLa_Pixel_Classifier.ilp --raw_data="',dir_path,'/TxRed_Reg/',well_location,'_TxRed_Scene1Interval*.tif" --output_format="hdf5" --output_filename_format=',dir_path,'/TxRed_Reg/Probabilities.h5'))
+  #system(paste0('srun -c 8 /home/users/dane/ilastik-1.3.2b3-Linux/run_ilastik.sh --headless --project=/graylab/share/dane/CellTracking/HeLa_Pixel_Classifier.ilp --raw_data="',dir_path,'/TxRed_Reg/',well_location,'_TxRed_Scene1Interval*.tif" --output_format="hdf5" --output_filename_format=',dir_path,'/TxRed_Reg/Probabilities.h5'))
   
-  system(paste0('srun -c 8 /home/users/dane/ilastik-1.3.2b3-Linux/run_ilastik.sh --headless --project=/graylab/share/dane/CellTracking/HeLa_Tracking.ilp --raw_data="',dir_path,'/TxRed_Reg/',well_location,'_TxRed_Scene1Interval*.tif" --prediction_maps=',dir_path,'/TxRed_Reg/Probabilities.h5 --export_source="Plugin" --export_plugin="CSV-Table"'))
+  system(paste0('srun -c 8 /home/users/dane/ilastik-1.3.2b3-Linux/run_ilastik.sh --headless --readonly=True --project=/graylab/share/dane/CellTracking/HeLa_Tracking.ilp --raw_data="',dir_path,'/TxRed_Reg/',well_location,'_TxRed_Scene1Interval*.tif" --prediction_maps=',dir_path,'/TxRed_Reg/Probabilities.h5 --export_source="Plugin" --export_plugin="CSV-Table"'))
   
-  system(paste0('srun -c 8 /home/users/dane/ilastik-1.3.2b3-Linux/run_ilastik.sh --headless --project=/graylab/share/dane/CellTracking/HeLa_Tracking.ilp --raw_data="',dir_path,'/GFP_Reg/',well_location,'_GFP_Scene1Interval*.tif" --prediction_maps=',dir_path,'/TxRed_Reg/Probabilities.h5 --export_source="Plugin" --export_plugin="CSV-Table"'))
+  system(paste0('srun -c 8 /home/users/dane/ilastik-1.3.2b3-Linux/run_ilastik.sh --headless --readonly=True --project=/graylab/share/dane/CellTracking/HeLa_Tracking.ilp --raw_data="',dir_path,'/GFP_Reg/',well_location,'_GFP_Scene1Interval*.tif" --prediction_maps=',dir_path,'/TxRed_Reg/Probabilities.h5 --export_source="Plugin" --export_plugin="CSV-Table"'))
 })
 
