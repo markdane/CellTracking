8f8bdabbb232d4df80c3fa93798a5ac055177392
markdane <dane@ohsu.edu>
2018-11-07 17:28:44 -0800

    Adding parallel execution on IncuCyte data

---
+++ ilastik_eppec_IncuCyte.R
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+library(stringr)
+#Cell this script from a directory with well_location subdirectories
+#that have R_Reg and G_Reg subdirectories
+#####DEBUG skipping over first two subdirectories
+pwd <- getwd()
+dirs <- paste0(pwd,"/",dir(pattern = "[[:alnum:]]*_[[:digit:]]*"))[4]
+#dir_path = "A3_1"
+
+# res <- lapply(dirs, function(dir_path){
+#   message("classifying pixels in nuclear images in: ",dir_path)
+#   well_location <- str_remove(dir_path,".*/")
+# 
+#   system(paste0('srun -c 8 /home/users/dane/ilastik-1.3.2b3-Linux/run_ilastik.sh --headless --project=/graylab/share/dane/CellTracking/IncuCyte_pixel_classifier.ilp --raw_data="',dir_path,'/R_Reg/R_Unreg*.tif" --output_format="hdf5" --output_filename_format=',dir_path,'/R_Reg/Probabilities.h5'),
+#          wait=TRUE)
+# })
+# 
+###put a wait in here then the rest of it can run in parallel
+res <- lapply(dirs, function(dir_path){
+  message("processing nuclear images in: ",dir_path)
+  well_location <- str_remove(dir_path,".*/")
+  system(paste0('srun -c 4 /home/users/dane/ilastik-1.3.2b3-Linux/run_ilastik.sh --headless --readonly=True --project=/graylab/share/dane/CellTracking/IncuCyte_tracking_classifier.ilp --raw_data="',dir_path,'/R_Reg/R_Unreg*.tif" --prediction_maps=',dir_path,'/R_Reg/Probabilities.h5 --export_source="Plugin" --export_plugin="CSV-Table"'),
+         wait=FALSE)
+})
+
+#   res <- lapply(dirs, function(dir_path){
+#     message("processing reporter images in: ",dir_path)
+#     well_location <- str_remove(dir_path,".*/")
+#   system(paste0('srun -c 4 /home/users/dane/ilastik-1.3.2b3-Linux/run_ilastik.sh --headless --readonly=True --project=/graylab/share/dane/CellTracking/IncuCyte_tracking_classifier.ilp --raw_data="',dir_path,'/G_Reg/G_Unreg*.tif" --prediction_maps=',dir_path,'/R_Reg/Probabilities.h5 --export_source="Plugin" --export_plugin="CSV-Table"'),
+#          wait=FALSE)
+# })
+
