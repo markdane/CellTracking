2698abd7377255b5c220ee6952bef776f21b0451
markdane <dane@ohsu.edu>
2018-12-13 09:09:24 -0800

    separate input and output data

--- ilastik_pixel_classifier.R
+++ ilastik_pixel_classifier.R
@@ -2,7 +2,7 @@
 library(stringr)
 
 #Create probability maps of pixels being in the nuclei
-#Input are composite images in P_Reg subdirectories
+#Input are composite images in the well_location subdirectories
 #Output are hf files in each well_location
 #Call this script from eppec and from directory with well_location subdirectories
 #that have a P_Reg subdirectories
@@ -18,7 +18,7 @@ res <- lapply(dirs, function(dir_path){
   well_location <- str_remove(dir_path,".*/")
 # Create pixel probability masks based on the high-contrast nuclear images and
 # store them in the P_Reg directories
-system(paste0('srun -c 4 /home/users/dane/ilastik-1.3.2b3-Linux/run_ilastik.sh --headless --readonly=True --project=/graylab/share/dane/CellTracking/HDF5_pixels.ilp --raw_data=',dir_path,'/P_Reg/Composite_reg.tif --output_format="hdf5" --output_filename_format=',dir_path,'/Probabilities.h5'),
+system(paste0('srun -c 4 /home/users/dane/ilastik-1.3.2b3-Linux/run_ilastik.sh --headless --readonly=True --project=/graylab/share/dane/CellTracking/HDF5_pixels.ilp --raw_data=',dir_path,'/Composite_reg.tif --output_format="hdf5" --output_filename_format=',dir_path,'/Probabilities.h5'),
        wait=FALSE)
 })
 
