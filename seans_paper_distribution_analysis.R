9d5148ad1680c76381d052941c17496d1d58a2ae
markdane <dane@ohsu.edu>
2019-02-01 16:24:29 -0800

    Added ggplot based plots

--- seans_paper_distribution_analysis.R
+++ seans_paper_distribution_analysis.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(readxl)
 library(tinytex)
 
-pdf(file = "Noise_distributions.pdf")
+pdf(file = "Noise_distributions_combined.pdf")
 X129_IGFDose_T0Norm_Noise <- read_excel("129_IGFDose_T0Norm_Noise.xlsx") %>%
   rename(IGF_I_0_pM = "1",
         IGF_I_5_pM = "2",
@@ -13,18 +13,39 @@ X129_IGFDose_T0Norm_Noise <- read_excel("129_IGFDose_T0Norm_Noise.xlsx") %>%
         IGF_I_40_pM = "8",
         IGF_I_125_pM = "11")
 
+df <- gather(X129_IGFDose_T0Norm_Noise, key = "Dosage", value = "Noise")
 
-qqplots <- function(x, col_name){
-  title_text <- str_replace(col_name, "_", "-") %>%
-    str_replace("_"," ") %>%
-    str_remove("_") 
-  qqnorm(x, pch = 20, main=paste("Normal QQ plot of", title_text))
-  qqline(x, col = "blue")
-  hist(x, main=paste("Histogram of",title_text), breaks = 100)
+p <- ggplot(df, aes(sample = Noise, colour = Dosage)) +
+  stat_qq(alpha = .5) +
+  stat_qq_line() +
+  theme_bw()
+plot(p)
 
-}
+p <- ggplot(df, aes(sample = Noise)) +
+  stat_qq(alpha = .5) +
+  stat_qq_line() +
+  theme_bw() +
+  facet_wrap(~Dosage)
+plot(p)
 
-res <- map2(X129_IGFDose_T0Norm_Noise, names(X129_IGFDose_T0Norm_Noise), qqplots)
+p <- ggplot(df, aes(x = Noise)) +
+geom_histogram(bins = 100) +
+  theme_bw() +
+  facet_wrap(~Dosage)
+plot(p)
+
+
+# qqplots <- function(x, col_name){
+#   title_text <- str_replace(col_name, "_", "-") %>%
+#     str_replace("_"," ") %>%
+#     str_remove("_") 
+#   qqnorm(x, pch = 20, main=paste("Normal QQ plot of", title_text))
+#   qqline(x, col = "blue")
+#   hist(x, main=paste("Histogram of",title_text), breaks = 100)
+# 
+# }
+# 
+# res <- map2(X129_IGFDose_T0Norm_Noise, names(X129_IGFDose_T0Norm_Noise), qqplots)
 
 dev.off()
 
