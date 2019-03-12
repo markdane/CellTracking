2850c4e7e2cc133f883cd51fda51e4b2633ac742
markdane <dane@ohsu.edu>
2019-03-12 07:00:35 -0700

    Set for parallel run

--- ilastik_pixel_classifier.R
+++ ilastik_pixel_classifier.R
@@ -12,13 +12,14 @@ library(stringr)
 # plate_path = commandArgs(trailingOnly=TRUE)
 plate_path <- "/eppec/storage/groups/heiserlab/image_scratch/AU_I_L_008_01_1"
 # dirs <- dir(path = plate_path, pattern = "[[:alnum:]]*_[[:digit:]]*",full.names = TRUE)[15]
-dirs <- dir(path = plate_path, pattern = "A1_1",full.names = TRUE)
+dirs <- dir(path = plate_path, pattern = "C5_4|[D][12346]_[124]",full.names = TRUE)
+#dirs <- dir(path = plate_path, pattern = "B1_4",full.names = TRUE)
 res <- lapply(dirs, function(dir_path){
   message("classifying pixels in: ",dir_path)
   well_location <- str_remove(dir_path,".*/")
 # Create pixel probability masks based on the high-contrast nuclear images and
 # store them in the P_Reg directories
 system(paste0('srun -c 8 --exclude=eppec-node6,eppec-node7 /home/users/dane/ilastik-1.3.2b3-Linux/run_ilastik.sh --headless --readonly=True --project=/graylab/share/dane/CellTracking/AU00801_Pixel_112918_plus_tracking.ilp --raw_data=',dir_path,'/Analysis/Composite_reg.tif --export_source="Probabilities" --output_format="hdf5" --output_filename_format=',dir_path,'/Analysis/Composite_reg_probs.h5'),
-       wait=TRUE)
+       wait=FALSE)
 })
 
