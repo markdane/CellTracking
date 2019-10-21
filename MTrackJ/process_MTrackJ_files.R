6c30b560eb45c9a88b7dd2a46c317fd73cbeb693
Mark Dane <you@example.com>
2019-10-21 15:30:05 -0700

    Clean up code Add paths to figures

--- MTrackJ/process_MTrackJ_files.R
+++ MTrackJ/process_MTrackJ_files.R
@@ -1,28 +1,22 @@
 library(tidyverse)
-library(knitr)
 library(rmarkdown)
 
 #assume there is a subfolder named Data that contains data files
 #with plateID_well alphanumeric_field_intensities.csv files
 
 process_files <- function(path){
-  filename <- str_remove(path, ".*/")
-  plateID <-  str_remove(filename, ".*/") %>%
-    str_remove( "_.*")
-  well <- str_extract(filename, "_[[:alpha:]][[:digit:]]*_") %>%
-    str_remove_all( "_")
-  field <- str_extract(filename, "_[[:digit:]]*_") %>%
-    str_remove_all("_")
   out_filename <- str_replace(path,"Points","intensities")
+  
   if(!dir.exists("Reports")) dir.create("Reports")
-  report_name <- paste0("Reports/",plateID,"_",well, "_", field,".html")
+  
+  report_name <- str_replace(path, "Data","Reports") %>%
+    str_replace("_Points.csv",".html")
   
   render("Manual_tracking_EDA.Rmd",
          output_file = report_name,
-         output_format = "html_document")
-  
+         output_format = "html_document",
+         output_options = list(code_folding = "hide"))
 }
  
-files_to_process <- dir("Data", pattern = "Points.csv", full.names = TRUE)
-
-res <- map(files_to_process, process_files)
+res <- dir("Data", pattern = "Points.csv", full.names = TRUE) %>%
+  map(process_files)
