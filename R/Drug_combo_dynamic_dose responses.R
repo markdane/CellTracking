914aaf027c81217bfebd7cf73e53c31790c80b3a
Mark Dane <dane@ohsu.edu>
2021-02-17 11:04:12 -0800

    Initial commit

---
+++ R/Drug_combo_dynamic_dose responses.R
@@ -0,0 +1,209 @@
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# R-Script for animating time-series data
+# Based on a script from Joachim Goedhart (@joachimgoedhart)
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+library(tidyverse)
+library(gganimate)
+library(magick)
+library(readxl)
+
+#functions####
+
+#read and prepare data
+read_process_data <- function(combo_name){
+  drug_names <- str_split(combo_name, pattern = "_", simplify = TRUE) %>%
+    tolower()
+  
+  cell <- read_excel(paste0("../data/",combo_name,"_CELL.xlsx")) %>%
+    janitor::clean_names() %>%
+    pivot_longer(cols = !time, names_to = "treatment", values_to = "counts")
+  
+  cycle <- read_excel(paste0("../data/",combo_name,"_CYCLE.xlsx")) %>%
+    janitor::clean_names()%>%
+    pivot_longer(cols = !time, names_to = "treatment", values_to = "G1_percent")
+  
+  return(list(cell = cell, cycle = cycle))
+}
+
+#Create static plots to help development
+plot_data <- function(cell_cycle_data, exclude_combo = TRUE){
+  nframes <- length(unique(cell_cycle_data$cell$time))
+  
+  #Choose a dark palette, and then replace the control and combo colors
+  #replace dark gray with black
+  treatment_colors <- RColorBrewer::brewer.pal(name = "Dark2", n = length(unique(cell_cycle_data$cell$treatment)))
+  names(treatment_colors) <- unique(cell_cycle_data$cell$treatment)
+  treatment_colors[["control"]] <- "black"
+  treatment_colors[["combo"]] <- "blue"
+  
+  if(exclude_combo){
+    df <- cell_cycle_data$cell %>%
+      filter(!treatment == "combo")
+  } else {
+    df <- cell_cycle_data$cell
+  }
+  
+  # Code for an ordinary plot
+  # p <- ggplot(df, aes(x=time, y=counts, color=treatment)) + 
+  #   geom_line(size=.5) + 
+  #   scale_x_continuous(breaks = c(0,24,48, 72, 96)) +
+  #   scale_color_manual(values = treatment_colors) +
+  #   lims(y = c(0,3.5)) +
+  #   labs(x = "Time (hours)",
+  #        y = "Cell counts (fold change)",
+  #        color = "Treatments") +
+  #   theme_light(base_size = 16)+
+  #   theme(panel.background = element_blank(),
+  #         #panel.border = element_blank(),
+  #         #panel.grid.major = element_blank(),
+  #         panel.grid.minor = element_blank()
+  #   )
+  # p
+  
+  # Generate the cell count animation
+  if(exclude_combo) {
+    cell_plot <- ggplot(df, aes(x=time, y=counts, color=treatment)) + 
+      geom_line(size=.7) +
+      geom_point(size=1) +
+      transition_reveal(time) +
+      scale_x_continuous(breaks = c(0,24,48, 72, 96)) +
+      scale_color_manual(values = treatment_colors) +
+      lims(y = c(0,3.5)) +
+      labs(x = "Time (hours)",
+           y = "Cell counts (fold change)",
+           color = "Treatments") +
+      theme_light(base_size = 16)+
+      theme(panel.background = element_blank(),
+            panel.grid.minor = element_blank(),
+            legend.position="none"
+      )
+  } else {
+    df_no_combo <- df %>%
+      filter(!treatment == "combo")
+    df_combo <- df  %>%
+      filter(treatment == "combo")
+    p <- ggplot(df_no_combo, aes(x=time, y=counts, color=treatment, label = treatment)) + 
+      geom_line(size=.7, alpha = .5) +
+      geom_text() +
+      shadow_mark(past = TRUE, future = TRUE, exclude_layer = c(2,5)) +
+      scale_x_continuous(breaks = c(0,24,48, 72, 96)) +
+      scale_color_manual(values = treatment_colors) +
+      lims(y = c(0,3.5)) +
+      labs(x = "Time (hours)",
+           y = "Cell counts (fold change)",
+           color = "Treatments") +
+      theme_light(base_size = 16)+
+      theme(panel.background = element_blank(),
+            panel.grid.minor = element_blank(),
+            legend.position="none"
+      )
+    cell_plot <- p + geom_line(data = df_combo, size=2, alpha = 1) +
+      geom_label(data = df_combo, aes(label = treatment)) +
+      transition_reveal(time) 
+    
+  }
+  
+  #start cell cycle plots
+  # Code for an ordinary plot
+  if(exclude_combo){
+    df <- cell_cycle_data$cycle %>%
+      filter(!treatment == "combo")
+  } else {
+    df <- cell_cycle_data$cycle
+  }
+  
+  # p <- ggplot(df, aes(x=time, y=G1_percent, color=treatment)) +
+  #   geom_line(size=.5) +
+  #   scale_x_continuous(breaks = c(0,24,48, 72, 96)) +
+  #   scale_color_manual(values = treatment_colors) +
+  #   lims(y = c(0,100)) +
+  #   labs(x = "Time (hours)",
+  #        y = "G1 percent",
+  #        color = "Treatments") +
+  #   theme_light(base_size = 16)+
+  #   theme(panel.background = element_blank(),
+  #         panel.grid.minor = element_blank()
+  #   )
+  # p
+  
+  # Generate the cell cycle phase animation
+  if(exclude_combo) {
+    cycle_plot <- ggplot(df, aes(x=time, y=G1_percent, color=treatment)) +
+      geom_line(size=.7) +
+      geom_point(size=1) +
+      transition_reveal(time) +
+      scale_x_continuous(breaks = c(0,24,48, 72, 96)) +
+      scale_color_manual(values = treatment_colors) +
+      lims(y = c(0,100)) +
+      labs(x = "Time (hours)",
+           y = "G1 percent",
+           color = "Treatments") +
+      theme_light(base_size = 16)+
+      theme(panel.background = element_blank(),
+            panel.grid.minor = element_blank()
+      )
+  } else {
+    df_no_combo <- df %>%
+      filter(!treatment == "combo")
+    df_combo <- df  %>%
+      filter(treatment == "combo")
+    p <- ggplot(df_no_combo, aes(x=time, y=G1_percent, color=treatment)) +
+      geom_line(size=.7, alpha = .5) +
+      shadow_mark(past = TRUE, future = TRUE, exclude_layer = 2) +
+      scale_x_continuous(breaks = c(0,24,48, 72, 96)) +
+      scale_color_manual(values = treatment_colors) +
+      lims(y = c(0,100)) +
+      labs(x = "Time (hours)",
+           y = "G1 percent",
+           color = "Treatments") +
+      theme_light(base_size = 16)+
+      theme(panel.background = element_blank(),
+            panel.grid.minor = element_blank()
+      )
+    cycle_plot <- p + geom_line(data = df_combo, size=2, alpha = 1) +
+      transition_reveal(time)
+    
+  }
+  
+  return(list(cell = cell_plot, cycle = cycle_plot))
+}
+
+#animate the plots
+animate_plots <- function(cell_cycle_plot){
+  nframes <- length(unique(cell_cycle_plot$cell$data$time))
+  panel_a <- animate(cell_cycle_plot$cell, nframes, height = 500, renderer = magick_renderer())
+  panel_b <- animate(cell_cycle_plot$cycle, nframes, height = 500, renderer = magick_renderer())
+  
+  #Create first frame then add the rest
+  full_gif <- image_append(c(panel_a[1], panel_b[1]))
+  for(i in 2:nframes){
+    next_frame <- image_append(c(panel_a[i], panel_b[i]))
+    full_gif <- c(full_gif, next_frame)
+  }
+  #Save the combined GIF
+  image_write_gif(full_gif, paste0("../plots/",names(cell_cycle_plot),"_combined.gif"))
+}
+
+#end of functions###
+
+#read the data files
+combo_names <- c("LAP100_PALBO100" = "LAP100_PALBO100",
+                 "GEM10_PALBO100" = "GEM10_PALBO100",
+                 "GEM10_LAP100" = "GEM10_LAP100",
+                 "GEM10_DOX20" = "GEM10_DOX20")
+cell_cycle_data <- map(combo_names, read_process_data)
+
+#generate the plots
+cell_cycle_plots <- map(cell_cycle_data, plot_data, exclude_combo = FALSE)
+
+#animate the plots
+res <- map(cell_cycle_plots, animate_plots)
+
+#Do a version with the combinations
+cell_cycle_plots <- map(cell_cycle_data, plot_data, exclude_combo = TRUE)
+
+#animate the plots
+res <- map(cell_cycle_plots, animate_plots)
+
+
--- R/Movie-with-plot.R
+++ R/Movie-with-plot.R
@@ -22,43 +22,58 @@ AU01101_02_CELL <- read_excel("data/AU01101-02_CELL.xlsx") %>%
   select(-index) %>%
   rename(time = time_1)
 
+selected_wells <- AU01101_02_CELL %>%
+  filter(plate == "AU01101",
+         well %in% c("a1", "a2", "a3", "a4", "a4", "a6"))
+
 #Read the number of frames
-nframes <- length(unique(AU01101_02_CELL$time))
+nframes <- length(unique(selected_wells$time))
 
 # Code for an ordinary plot - uncomment to have to inspect the data
-p <- ggplot(AU01101_02_CELL, aes(x=time, y=value, color=well)) + 
-  geom_line(size=2) + 
-  geom_point(size=3) +
-  facet_wrap(~plate)
+p <- ggplot(selected_wells, aes(x=time, y=value, color=well)) + 
+  geom_line(size=.5) + 
+  #geom_point(size=3) +
+  scale_x_continuous(breaks = c(0,24,48, 72, 96)) +
+  facet_wrap(~plate, nrow = 3, strip.position = "right") +
+  theme_light(base_size = 16)+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position="none",
+        strip.background =element_rect(fill=NA, color='grey'))
 p
 
 
 # Generate the animation
-animated_plot <- ggplot(AU01101_02_CELL, aes(x=time, y=value, color=well)) + geom_line(size=.5) + geom_point(size=1) + transition_reveal(time) +
-  facet_wrap(~plate)
+animated_plot <- ggplot(selected_wells, aes(x=time, y=value, color=well)) + 
+  geom_line(size=.5) +
+  geom_point(size=1) +
+  transition_reveal(time) +
+  scale_x_continuous(breaks = c(0,24,48, 72, 96)) +
+  facet_wrap(~plate, nrow = 3, strip.position = "right") +
+  theme_light(base_size = 16)+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position="none",
+        strip.background =element_rect(fill=NA, color='grey'))
 
-#Do some formatting of the layout
-animated_plot <- animated_plot + theme_light(base_size = 16)
-animated_plot <- animated_plot + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-animated_plot <- animated_plot + theme(legend.position="none")
-animated_plot <- animated_plot + facet_wrap(~plate, nrow = 3, strip.position = "right") + 
-  theme(strip.background =element_rect(fill=NA, color='grey')) + 
-  theme(strip.text = element_text(colour = 'black'))
+nframes <- 97
+# Uncomment the next line to save the animated plot as GIF
+#image_write_gif(animate(animated_plot, nframes, renderer = magick_renderer()), 'data_animation.gif')
 
-# Uncommen the next line to save the animated plot as GIF
-image_write_gif(animate(animated_plot, nframes, renderer = magick_renderer()), 'animation.gif')
-
-panel_b <- animate(animated_plot, nframes, height = 529, renderer = magick_renderer())
+panel_b <- animate(animated_plot, nframes, height = 1024, renderer = magick_renderer())
 
 panel_a <- image_read("LI802303_A02_2_treatment.gif")
 
-combined_gif <- image_append(c(panel_a[1], panel_b[1]))
+last_frame <- image_append(c(panel_a[nframes], panel_b[nframes]))
 
 #debug 
-nframes <- 97
+nframes = 5
+#Create first frame
+two_movies <- image_append(c(panel_a[1], panel_a[1])) 
+combined_gif <- image_append(c(two_movies, panel_b[1]), stack = TRUE)
 for(i in 2:nframes){
-  combined_panel <- image_append(c(panel_a[i], panel_b[i]))
-  combined_gif <- c(combined_gif, combined_panel)
+  two_movies <- image_append(c(panel_a[i], panel_a[i])) 
+  combined_gif <- image_append(c(two_movies, panel_b[i]), stack = TRUE)
 }
 
 #Save the combined GIF
