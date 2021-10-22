f9cdc3e984347a35cdea4d28a811abf78deb0293
markdane <dane@ohsu.edu>
2021-10-22 10:56:16 -0700

    begin linking pipeline modules

--- Live_cell_imaging_file_copy.py
+++ Live_cell_imaging_file_copy.py
@@ -10,16 +10,17 @@
 import pandas as pd
 import numpy as np
 import os, re, glob, shutil, sys
+from skimage import io
 
 
-plate_name = sys.argv[2]
+plateID = sys.argv[2]
 directory_name = sys.argv[1]
 #directory_name = "AU_I_L_015_02_1"
-#plate_name = "AU01502"
+#plateID = "AU01502"
 
-def get_file_df(plate_name):
-    print("getting files for "+plate_name)
-    data_path =  '/home/groups/graylab_share/OMERO.rdsStore/liby/AU565 Drug Screen Project/'+directory_name+'/'+plate_name+'_IMAGES'
+def get_file_df(plateID):
+    print("getting files for "+plateID)
+    data_path =  '/home/groups/graylab_share/OMERO.rdsStore/liby/AU565_Drug_Screen_Project/'+directory_name+'/'+plateID+'_IMAGES'
     data_paths = glob.glob(os.path.join(data_path,"*.tif"),recursive=False)
     df = pd.DataFrame(data_paths, columns =['path'])
     df['filename'] = df['path'].str.extract('(\w*.tif)')
@@ -47,13 +48,94 @@ def make_directories(df, dest_path):
         
 dest_path = '/home/exacloud/gscratch/HeiserLab/images/'
 
-print("starting file copy for "+plate_name)
-df = get_file_df(plate_name)
+print("starting file copy for "+plateID)
+df = get_file_df(plateID)
 df_directories = make_directories(df, dest_path)
     
 df = df.join(df_directories.set_index(['plateID','well','field']),on = ['plateID','well','field'])
-print("copying files to "+dest_path+'/'+plate_name)
+print("copying files to "+dest_path+'/'+plateID)
 for count, src_path in enumerate(df['path'].tolist()):
     full_dest_path = os.path.join(dest_path, df['dest_path'][count])
     shutil.copy2(src_path, full_dest_path)
-print("done copying files for "+plate_name+"\n")
+print("done copying files for "+plateID+"\n")
+
+pipeline_name = "PI" #python and ilastik
+data_path = '/home/exacloud/gscratch/HeiserLab/images/'
+output_path = os.path.join(data_path+plateID,"Analysis",pipeline_name,"intermediate_files")
+subdirectories = sorted(glob.glob(os.path.join(data_path+plateID,"[A-Z][1-9]","field_[1-9]")))
+
+# Only process the files if there is no stack of RGP files
+
+flourescent_scaler = 255/4095 #rescale from 12 to 8 bits
+for subdir in subdirectories:
+    well = re.findall("/[A-Z][1-9]",subdir)[0]
+    well = re.findall("[A-Z][1-9]", well)[0]
+    field = re.findall("field_[1-9]",subdir)[0]
+    field_num = re.findall("[0-9]", field)[0]
+    c_filename = os.path.join(output_path,plateID+"_RGP_"+well+"_"+field_num+"_stack.tif")
+    metadata_filename = os.path.join(output_path,plateID+"_RGP_"+well+"_"+field_num+"_filenames.txt")
+    if not os.path.exists(c_filename):
+        print("combining channels in "+subdir)
+        #load and prepare red, green and phase channels. Scale for 8 bits but these are uint16 data types
+        r_data_paths = glob.glob(os.path.join(subdir,"*_R_*m.tif"))
+        r_time_slices = set()
+        for data_paths in r_data_paths:
+            r_time_slices.add(re.findall("..d..h..m", data_paths)[0])
+        g_data_paths = glob.glob(os.path.join(subdir,"*_G_*m.tif"))
+        g_time_slices = set()
+        for data_paths in g_data_paths:
+            g_time_slices.add(re.findall("..d..h..m", data_paths)[0])
+        p_data_paths = glob.glob(os.path.join(subdir,"*_P_*m.tif"))
+        p_time_slices = set()
+        for data_paths in p_data_paths:
+            p_time_slices.add(re.findall("..d..h..m", data_paths)[0])
+        complete_time_slices = r_time_slices & g_time_slices & p_time_slices
+        r_data_paths_c = []
+        g_data_paths_c = []
+        p_data_paths_c = []
+        for time_slice in complete_time_slices:
+            r_data_paths_c.append(os.path.join(data_path+plateID,well,field,plateID+"_R_"+well+"_"+field_num+"_"+time_slice+".tif"))
+            g_data_paths_c.append(os.path.join(data_path+plateID,well,field,plateID+"_G_"+well+"_"+field_num+"_"+time_slice+".tif"))
+            p_data_paths_c.append(os.path.join(data_path+plateID,well,field,plateID+"_P_"+well+"_"+field_num+"_"+time_slice+".tif"))
+        img_r_ic = io.imread_collection(r_data_paths_c) # 3 dimensions : frames x width x height
+        img_rs = np.stack(img_r_ic)*flourescent_scaler
+
+        img_g_ic = io.imread_collection(g_data_paths_c) # 3 dimensions : frames x width x height
+        img_gs = np.stack(img_g_ic)*flourescent_scaler
+
+        img_p_ic = io.imread_collection(p_data_paths_c) # 3 dimensions : frames x width x height
+        img_ps = np.stack(img_p_ic)
+
+        img_c = np.stack([img_rs.astype('B'), img_gs.astype('B'), img_ps.astype('B')], axis = -1) 
+    
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+
+        io.imsave(c_filename, img_c, plugin='tifffile')
+        with open(metadata_filename, 'w') as filehandle:
+            for listitem in img_r_ic.files:
+                filehandle.write('%s\n' % re.sub("_R_","_RGP_",listitem))
+
+input_files_path = os.path.join(data_path+plateID,"Analysis",pipeline_name,"intermediate_files")
+image_stack_paths = sorted(glob.glob(input_files_path+"/*stack.tif"))
+mask_paths = sorted(glob.glob(input_files_path+"/*Segmentation.h5"))
+mainpath = os.path.join(data_path,plateID,"Analysis",pipeline_name)
+
+# Only create an  job if there is no output file.
+
+wells = sorted(set(re.findall(r"_[A-Z][1-9]_", ''.join(mask_paths)))) #get a unique set of the wells with images that have been segmented
+
+for well in wells:
+    #initate a job if the csv file for the current well does not exists
+    l1_file_path = data_path+plateID+"/Analysis/"+pipeline_name+"/"+plateID+well+"level_1.csv"
+    well = re.sub("_","",well)
+    if not os.path.exists(l1_file_path):
+        cmd = 'srun -c 8 -J M'+plateID[3:7]+well[0:2]+' -o M'+plateID[3:7]+well[0:2]+'_out.txt -t 23:00:00 python Apply_ilastik_masks.py '+plateID+' '+well+' &'
+        print("launching job with command "+cmd)
+        returned_value = os.system(cmd)  # returns the exit code in unix
+        if returned_value == 0:
+            print("launched job to create data for "+l1_file_path)
+        else:
+            print("failed to launch job to create data for "+l1_file_path)
+
+
