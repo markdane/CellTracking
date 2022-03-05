7367c93f85f486680b34e7d9e695f5df64972cac
markdane <dane@ohsu.edu>
2022-03-05 09:39:09 -0800

    tracking sys command

--- bash/cellpose_GPU_sbatch.sh
+++ bash/cellpose_GPU_sbatch.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres gpu:1
 #SBATCH --mem 40G
 #SBATCH --time 23:00:00
-#SBATCH --job-name 2001
-#SBATCH --array=1-1
+#SBATCH --job-name 2001410
+#SBATCH --array=4-10
 
 python /home/exacloud/gscratch/HeiserLab/software/image_analysis_pipelines/AU565/python/Live_cell_imaging_cellpose_KIT.py AU02001 $SLURM_ARRAY_TASK_ID
