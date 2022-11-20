75c29dc310fe70369be95cd3601a0a5fa9c2b866
markdane <dane@ohsu.edu>
2022-11-20 10:25:35 -0800

    Fix bug in identifying existing files

--- python/Live_cell_imaging_file_copy.py
+++ python/Live_cell_imaging_file_copy.py
@@ -3,24 +3,28 @@
 
 # ### Live Cell Imaging File Copy
 # 
-# This script intiates processing AU565 image files by first copying them to the exacloud gscratch storage.
+# This notebook intiates processing  image files by first copying them to the exacloud gscratch storage.
+
+# In[2]:
 
 
 #setup libraries
-import pandas as pd
 import numpy as np
+import pandas as pd
 import os, re, glob, shutil, sys
-from skimage import io
 
 
-plateID = sys.argv[2]
-directory_name = sys.argv[1]
-#directory_name = "AU_I_L_015_02_1"
-#plateID = "AU01502"
+# In[ ]:
+
 
 def get_file_df(plateID):
     print("getting files for "+plateID)
-    data_path =  '/home/groups/graylab_share/OMERO.rdsStore/liby/AU565_Drug_Screen_Project/'+directory_name+'/'+plateID+'_IMAGES'
+    #data_path =  '/home/groups/graylab_share/OMERO.rdsStore/liby/'+directory_name+'/'+plateID+"/"+plateID+'_IMAGES'
+    #data_path =  '/home/groups/heiserlab_genomics/home/liby/'+directory_name+'/'+plateID+"/"+plateID+'_IMAGES'
+    data_path =  '/home/groups/heiserlab_genomics/home/liby/'+directory_name+'/'+plateID+'_IMAGES' #Use for HCC1143 images
+    #data_path = "/home/groups/heiserlab_genomics/home/grossse/AU565 Project/AU565 Image Files/AU_I_L_006_01_1/AU_I_L_006_01_1_IMAGES"
+    #data_path =   '/home/groups/heiserlab_genomics/globus/calistri/'+directory_name+'/'+plateID
+
     data_paths = glob.glob(os.path.join(data_path,"*.tif"),recursive=False)
     df = pd.DataFrame(data_paths, columns =['path'])
     df['filename'] = df['path'].str.extract('(\w*.tif)')
@@ -33,6 +37,24 @@ def get_file_df(plateID):
     df['field'] = df['field'].str.extract('([0-9]+)')
     df['time'] = df['filename'].str.extract('(_[a-z0-9]*.tif)')
     df['time'] = df['time'].str.replace('[_(.tif)]','',regex = True)
+    
+    print("get existing files")
+    existing_file_paths = glob.glob(os.path.join(dest_path,plateID,"[A-D][1-6]","field_[1-4]","*.tif"),recursive=False)
+    existing_df = pd.DataFrame(existing_file_paths, columns =['path'])
+    existing_df['filename'] = existing_df['path'].str.extract('(\w*.tif)')
+    existing_df['plateID'] = existing_df['filename'].str.extract('(^[a-zA-Z0-9]*)')
+    existing_df['channel'] = existing_df['filename'].str.extract('(_[PRG]_)')
+    existing_df['channel'] = existing_df['channel'].str.extract('([PRG])')
+    existing_df['well'] = existing_df['filename'].str.extract('(_[A-Z][0-9]+_)')
+    existing_df['well'] = existing_df['well'].str.extract('([A-Z][0-9]+)')
+    existing_df['field'] = existing_df['filename'].str.extract('(_[0-9]+_)')
+    existing_df['field'] = existing_df['field'].str.extract('([0-9]+)')
+    existing_df['time'] = existing_df['filename'].str.extract('(_[a-z0-9]*.tif)')
+    existing_df['time'] = existing_df['time'].str.replace('[_(.tif)]','',regex = True)
+    #delete existing file paths from df to not waste time overwriting them
+    #only execute if there are existing files
+    if(existing_df.shape[0] >0):
+        df = df.loc[np.isin(df['filename'].to_numpy(), existing_df['filename'].to_numpy(), assume_unique=True, invert= True)] 
     return df
 
 def make_directories(df, dest_path):
@@ -45,97 +67,66 @@ def make_directories(df, dest_path):
         if not os.path.exists(full_path):
             os.makedirs(full_path)
     return df_directories 
-        
+
+def get_dest_df(dest_path):
+    print("get existing files")
+    existing_file_paths = glob.glob(os.path.join(dest_path,plate_name,"[A-D][1-6]","field_[1-4]","*.tif"),recursive=False)
+
+
 dest_path = '/home/exacloud/gscratch/HeiserLab/images/'
+directory_name = "AU565_drug_screen"
+directory_name = "HCC1143_drug_screen"
+#directory_name = "21MT1_drug_screen"
+#directory_name = "MDAMB157_drug_screen"
+#directory_name = "AU_I_L_006_01_1"
+#directory_name = "nlc_incucyte"
+#directory_name = "AU565_Drug_Screen_Project" #used for files stored on graylab_share
 
-print("starting file copy for "+plateID)
-df = get_file_df(plateID)
-df_directories = make_directories(df, dest_path)
-    
-df = df.join(df_directories.set_index(['plateID','well','field']),on = ['plateID','well','field'])
-print("copying files to "+dest_path+'/'+plateID)
-for count, src_path in enumerate(df['path'].tolist()):
-    full_dest_path = os.path.join(dest_path, df['dest_path'][count])
-    shutil.copy2(src_path, full_dest_path)
-print("done copying files for "+plateID+"\n")
+#plateID = "AU00601"
+#plateID = "HC00701"
+#plateID = "2101401"
+#plateID = "MD00201"
+#plateID = "221006_nlc_hcc1143ccrep_sirna_ptx_p2"
 
-pipeline_name = "PI" #python and ilastik
-data_path = '/home/exacloud/gscratch/HeiserLab/images/'
-output_path = os.path.join(data_path+plateID,"Analysis",pipeline_name,"intermediate_files")
-subdirectories = sorted(glob.glob(os.path.join(data_path+plateID,"[A-Z][1-9]","field_[1-9]")))
+plateIDs = ["HC00701","HC00801","HC00901","HC01001","HC01301","HC01401"]
+plateIDs = ["HC00901","HC01001","HC01301","HC01401"]
+#plateIDs = ["2101001","2101201","2101301","2101401","2101501","2101601","2101701","2101801"]
+#plateIDs = ["MD00301","MD00401","MD00501","MD00601","MD00701","MD00801"]
 
-# Only process the files if there is no stack of RGP files
+for plateID in plateIDs:
+    print("starting file copy for "+plateID)
+    df_all = get_file_df(plateID)
+    #poor_quality_wells = ("A5", "A6", "B5", "B6","C5", "C6","D5", "D6")
+    #poor_quality_wells = ("A1", "A2", "A3", "A4", "A5","A6",
+    #                      "B1", "B2", "B3", "B4", "B5", "B6", 
+    #                      "C1", "C2",       "C4", "C5", "C5",
+    #                      "D1", "D2", "D3", "D4", "D5", "D6")
 
-flourescent_scaler = 255/4095 #rescale from 12 to 8 bits
-for subdir in subdirectories:
-    well = re.findall("/[A-Z][1-9]",subdir)[0]
-    well = re.findall("[A-Z][1-9]", well)[0]
-    field = re.findall("field_[1-9]",subdir)[0]
-    field_num = re.findall("[0-9]", field)[0]
-    c_filename = os.path.join(output_path,plateID+"_RGP_"+well+"_"+field_num+"_stack.tif")
-    metadata_filename = os.path.join(output_path,plateID+"_RGP_"+well+"_"+field_num+"_filenames.txt")
-    if not os.path.exists(c_filename):
-        print("combining channels in "+subdir)
-        #load and prepare red, green and phase channels. Scale for 8 bits but these are uint16 data types
-        r_data_paths = glob.glob(os.path.join(subdir,"*_R_*m.tif"))
-        r_time_slices = set()
-        for data_paths in r_data_paths:
-            r_time_slices.add(re.findall("..d..h..m", data_paths)[0])
-        g_data_paths = glob.glob(os.path.join(subdir,"*_G_*m.tif"))
-        g_time_slices = set()
-        for data_paths in g_data_paths:
-            g_time_slices.add(re.findall("..d..h..m", data_paths)[0])
-        p_data_paths = glob.glob(os.path.join(subdir,"*_P_*m.tif"))
-        p_time_slices = set()
-        for data_paths in p_data_paths:
-            p_time_slices.add(re.findall("..d..h..m", data_paths)[0])
-        complete_time_slices = r_time_slices & g_time_slices & p_time_slices
-        r_data_paths_c = []
-        g_data_paths_c = []
-        p_data_paths_c = []
-        for time_slice in complete_time_slices:
-            r_data_paths_c.append(os.path.join(data_path+plateID,well,field,plateID+"_R_"+well+"_"+field_num+"_"+time_slice+".tif"))
-            g_data_paths_c.append(os.path.join(data_path+plateID,well,field,plateID+"_G_"+well+"_"+field_num+"_"+time_slice+".tif"))
-            p_data_paths_c.append(os.path.join(data_path+plateID,well,field,plateID+"_P_"+well+"_"+field_num+"_"+time_slice+".tif"))
-        img_r_ic = io.imread_collection(r_data_paths_c) # 3 dimensions : frames x width x height
-        img_rs = np.stack(img_r_ic)*flourescent_scaler
+    poor_quality_wells = ()
 
-        img_g_ic = io.imread_collection(g_data_paths_c) # 3 dimensions : frames x width x height
-        img_gs = np.stack(img_g_ic)*flourescent_scaler
+    df = df_all[~df_all.well.isin(poor_quality_wells)].reset_index()
+    df_directories = make_directories(df, dest_path)
 
-        img_p_ic = io.imread_collection(p_data_paths_c) # 3 dimensions : frames x width x height
-        img_ps = np.stack(img_p_ic)
+    df = df.join(df_directories.set_index(['plateID','well','field']),on = ['plateID','well','field'])
+    df['full_dest_path'] = dest_path+df['dest_path']+"/"+df['filename']
+    df_copied = glob.glob(os.path.join(dest_path,plateID,"*/field*/*.tif"),recursive=True)
+    df_copy = df[~df['full_dest_path'].isin(df_copied)]
 
-        img_c = np.stack([img_rs.astype('B'), img_gs.astype('B'), img_ps.astype('B')], axis = -1) 
-    
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
+    attempts = 0
+    while (np.logical_and(len(df_copy) >0, attempts < 4)):
+        print("copying files to "+dest_path+plateID)
+        for count, src_path in enumerate(df_copy['path'].tolist()):
+            shutil.copy2(src_path, df_copy.iloc[count]['full_dest_path'])
+        #get files successfully copied
+        df_copied = glob.glob(os.path.join(dest_path,plateID,"*/field*/*.tif"),recursive=True)
+        #update dataframe of files that still need to be copied
+        df_copy = df_copy[~df_copy['full_dest_path'].isin(df_copied)]
+        attempts += 1
+    print("done copying files for "+plateID+"\n")
 
-        io.imsave(c_filename, img_c, plugin='tifffile')
-        with open(metadata_filename, 'w') as filehandle:
-            for listitem in img_r_ic.files:
-                filehandle.write('%s\n' % re.sub("_R_","_RGP_",listitem))
 
-input_files_path = os.path.join(data_path+plateID,"Analysis",pipeline_name,"intermediate_files")
-image_stack_paths = sorted(glob.glob(input_files_path+"/*stack.tif"))
-mask_paths = sorted(glob.glob(input_files_path+"/*Segmentation.h5"))
-mainpath = os.path.join(data_path,plateID,"Analysis",pipeline_name)
+# In[ ]:
 
-# Only create an  job if there is no output file.
 
-wells = sorted(set(re.findall(r"_[A-Z][1-9]_", ''.join(mask_paths)))) #get a unique set of the wells with images that have been segmented
-
-for well in wells:
-    #initate a job if the csv file for the current well does not exists
-    l1_file_path = data_path+plateID+"/Analysis/"+pipeline_name+"/"+plateID+well+"level_1.csv"
-    well = re.sub("_","",well)
-    if not os.path.exists(l1_file_path):
-        cmd = 'srun -c 8 -J M'+plateID[3:7]+well[0:2]+' -o M'+plateID[3:7]+well[0:2]+'_out.txt -t 23:00:00 python Apply_ilastik_masks.py '+plateID+' '+well+' &'
-        print("launching job with command "+cmd)
-        returned_value = os.system(cmd)  # returns the exit code in unix
-        if returned_value == 0:
-            print("launched job to create data for "+l1_file_path)
-        else:
-            print("failed to launch job to create data for "+l1_file_path)
 
 
